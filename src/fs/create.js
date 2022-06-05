a8dcd354588dd6dabd888959c47c8c86ce97b06e
roboticstechnology <roboticstechnology@gmail.com>
2022-06-05 22:01:35 +0300

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,17 @@
+import { writeFile, stat } from 'fs/promises';
+import { fileURLToPath } from 'url'
+import { dirname, basename } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// console.log(__filename);
+console.log(__dirname);
+
 export const create = async () => {
     // Write your code here 
+
+    writeFile(`${__dirname}/files/fresh.txt`, 'I am fresh and young');
+
 };
+
+create();
